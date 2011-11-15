ee4accd1c2ce067a7a91246bdc8355826247f431
Peter Shih <ptshih@gmail.com>
2011-11-15 01:33:24 -0500

    Adding demo project

---
+++ demo/PSKitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PSKitDemo
+//
+//  Created by Peter Shih on 11/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
