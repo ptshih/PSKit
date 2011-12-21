6e55a747e1b973d54e2881e953776a66e064aaa0
Peter Shih <ptshih@gmail.com>
2011-12-21 16:22:32 -0500

    adding a pswindow class

---
+++ src/PSKit/PSWindow.h
@@ -0,0 +1,13 @@
+//
+//  PSWindow.h
+//  OSnap
+//
+//  Created by Peter Shih on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSWindow : UIWindow
+
+@end
---
+++ src/PSKit/PSWindow.m
@@ -0,0 +1,21 @@
+//
+//  PSWindow.m
+//  OSnap
+//
+//  Created by Peter Shih on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PSWindow.h"
+
+@implementation PSWindow
+
+- (void)becomeKeyWindow {
+  [super becomeKeyWindow];  
+}
+
+- (void)resignKeyWindow {
+  [super resignKeyWindow];
+}
+
+@end
