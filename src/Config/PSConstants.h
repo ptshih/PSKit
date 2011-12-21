6acfb63ecd7930cc0d79de9956a1df3310c63050
Peter Shih <ptshih@gmail.com>
2011-12-21 16:22:12 -0500

    new PSConstants

--- src/Config/PSConstants.h
+++ src/Config/PSConstants.h
@@ -22,49 +22,21 @@
 // Import PSKit Categories
 #import "PSCategories.h"
 
-// Import any project-specific constants here
-#import "AppDelegate.h"
-
 #ifdef __APPLE__
 #include "TargetConditionals.h"
 #endif
 
-#define RADIANS(degrees) ((degrees * M_PI) / 180.0)
-
-// App Delegate Macro
-#define APP_DELEGATE ((AppDelegate *)[[UIApplication sharedApplication] delegate])
+/**
+ Device
+ */
+#define IS_DEVICE_PHONE ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone)
+#define IS_DEVICE_RETINA ([[UIScreen mainScreen] respondsToSelector:@selector(scale)] && [[UIScreen mainScreen] scale] == 2)
+#define IS_DEVICE_MULTITASKING ([[UIDevice currentDevice] multitaskingSupported])
 
 /**
  Alert Tags
  */
 #define kAlertFacebookPermissionsPublish 7001
-
-/**
- Locale/Language
- */
-#define USER_LANGUAGE [[NSLocale preferredLanguages] objectAtIndex:0]
-#define USER_LOCALE [[NSLocale autoupdatingCurrentLocale] localeIdentifier]
-
-/**
- Core Data (FILL THIS IN LOCAL CONSTANTS)
- */
-//#define CORE_DATA_SQL_FILE
-//#define CORE_DATA_MOM
-
-#define RGBCOLOR(R,G,B) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:1.0]
-#define RGBACOLOR(R,G,B,A) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
-
-/**
- Font Defines
- */
-#define PS_CAPTION_FONT [UIFont fontWithName:@"HelveticaNeue-Bold" size:12.0]
-#define PS_TITLE_FONT [UIFont fontWithName:@"HelveticaNeue-Bold" size:16.0]
-#define PS_LARGE_FONT [UIFont fontWithName:@"HelveticaNeue" size:16.0]
-#define PS_NORMAL_FONT [UIFont fontWithName:@"HelveticaNeue" size:14.0]
-#define PS_BOLD_FONT [UIFont fontWithName:@"HelveticaNeue-Bold" size:14.0]
-#define PS_SUBTITLE_FONT [UIFont fontWithName:@"HelveticaNeue" size:12.0]
-#define PS_TIMESTAMP_FONT [UIFont fontWithName:@"HelveticaNeue" size:10.0]
-#define PS_NAV_BUTTON_FONT [UIFont fontWithName:@"HelveticaNeue-Bold" size:12.0]
 
 /**
  Notifications
@@ -75,6 +47,48 @@
 #define kApplicationSuspended @"ApplicationSuspended"
 #define kCoreDataDidReset @"CoreDataDidReset"
 
+
+// Math Macros
+#define RADIANS(degrees) ((degrees * M_PI) / 180.0)
+#define DEGREES_TO_RADIANS(x) (M_PI * (x) / 180.0)
+#define RADIANS_TO_DEGREES(x) ((x) * 180.0 / M_PI)
+#define CLAMP(x, min, max) (MAX((min), MIN((max), (x))))
+
+#define CENTERED_WITHIN(smaller, bigger) (floor(((bigger) - (smaller)) / 2.0))
+#define HOURS_AGO(x) (60.0 * 60.0 * (x) * -1.0)
+
+// Convenience Macros
+#define APP [UIApplication sharedApplication]
+#define APP_DELEGATE [[UIApplication sharedApplication] delegate]
+#define APP_DEFAULTS [NSUserDefaults standardUserDefaults]
+#define APP_BUNDLE [NSBundle mainBundle]
+#define APP_FILEMANAGER [NSFileManager defaultManager]
+#define APP_SCREEN [UIScreen mainScreen]
+#define APP_FRAME (CGRect)[APP_SCREEN applicationFrame]
+#define APP_BOUNDS (CGRect)[APP_SCREEN bounds]
+
+
+// Directory Macros
+#define APP_DOCUMENTS [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject]
+#define APP_CACHES [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject]
+
+/**
+ View Macros
+ */
+#define UIViewAutoresizingFlexibleAllMargins (UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin)
+#define UIViewAutoresizingFlexibleSize (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight)
+
+/**
+ Locale/Language
+ */
+#define USER_LANGUAGE [[NSLocale preferredLanguages] objectAtIndex:0]
+#define USER_LOCALE [[NSLocale autoupdatingCurrentLocale] localeIdentifier]
+
+/**
+ Colors
+ */
+#define RGBCOLOR(R,G,B) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:1.0]
+#define RGBACOLOR(R,G,B,A) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
 
 /**
  Logging Macros
@@ -98,18 +112,28 @@
 /**
  Safe Releases
  */
-#define RELEASE_SAFELY(__POINTER) { [__POINTER release]; __POINTER = nil; }
+#define RELEASE_SAFELY(__POINTER) { if (nil != (__POINTER)) { [__POINTER release]; __POINTER = nil; } }
+#define RELEASE_CF_SAFELY(__REF) { if (nil != (__REF)) { CFRelease(__REF); __REF = nil; } }
 #define INVALIDATE_TIMER(__TIMER) { [__TIMER invalidate]; __TIMER = nil; }
 #define INVALIDATE_AND_RELEASE_TIMER(__TIMER) { [__TIMER invalidate]; [__TIMER release]; __TIMER = nil; }
-// Release a CoreFoundation object safely.
-#define RELEASE_CF_SAFELY(__REF) { if (nil != (__REF)) { CFRelease(__REF); __REF = nil; } }
+
+#define CLEAR_DELEGATES(__REF) { if (__REF.delegate != nil) { __REF.delegate = nil; } }
+#define CLEAR_DATASOURCES(__REF) { if (__REF.dataSource != nil) { __REF.dataSource = nil; } }
 
 /**
- Is Yelp installed?
+ Regex
  */
-static BOOL isYelpInstalled() {
-  return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"yelp:"]];
-}
+// Gruber's regex for finding URLs in a string (http://daringfireball.net/2009/11/liberal_regex_for_matching_urls)
+//
+// \b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))
+//
+// Also with the help of Apple's regular expression guide:
+//
+// http://developer.apple.com/iphone/library/documentation/General/Conceptual/iPadProgrammingGuide/Text/Text.html
+//
+#define kFLURLRegex (@"\\b(([\\w-]+://?|www[.])[^\\s()<>]+(?:\\([\\w\\d]+\\)|([^[:punct:]\\s]|/)))")
+#define kFLAtReplyRegex  (@"\\B(\\@\\w*|\\s+$)") // Regex for getting @replies in tweets
+#define kFLHashtagRegex  (@"\\B(\\#\\w*|\\s+$)") // Regex for getting #hashtags in tweets
 
 /**
  Is Multitasking supported?
@@ -128,4 +152,11 @@ static BOOL isDeviceIPad() {
   }
 #endif
   return NO;
+}
+
+/**
+ Is Yelp installed?
+ */
+static BOOL isYelpInstalled() {
+  return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"yelp:"]];
 }
