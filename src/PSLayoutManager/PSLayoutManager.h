c2892afda2ecc838edf062eadc574920b85f6385
Peter Shih <peter@foursquare.com>
2012-02-29 20:48:47 -0500

    stub pslayoutmanager

---
+++ src/PSLayoutManager/PSLayoutManager.h
@@ -0,0 +1,15 @@
+//
+//  PSLayoutManager.h
+//  PSKit
+//
+//  Created by Peter on 2/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PSLayoutManager : NSObject
+
++ (id)defaultManager;
+
+@end
---
+++ src/PSLayoutManager/PSLayoutManager.m
@@ -0,0 +1,32 @@
+//
+//  PSLayoutManager.m
+//  PSKit
+//
+//  Created by Peter on 2/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PSLayoutManager.h"
+
+@implementation PSLayoutManager
+
++ (id)defaultManager {
+    static id defaultManager;
+    if (!defaultManager) {
+        defaultManager = [[self alloc] init];
+    }
+    return defaultManager;
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
