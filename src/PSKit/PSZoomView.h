0decc244cabd2503e8b98156fa241bd42bec85bb
Peter Shih <ptshih@gmail.com>
2012-03-13 03:59:01 -0500

    pszoomview refactor

--- src/PSKit/PSZoomView.h
+++ src/PSKit/PSZoomView.h
@@ -12,11 +12,6 @@
 
 + (id)sharedView;
 
-- (id)initWithView:(UIView *)view superView:(UIView *)superView;
-- (id)initWithMapView:(MKMapView *)mapView mapRegion:(MKCoordinateRegion)mapRegion superView:(UIView *)superView;
-- (id)initWithImage:(UIImage *)image contentMode:(UIViewContentMode)contentMode;
-
-
 + (void)showMapView:(MKMapView *)mapView withFrame:(CGRect)frame inView:(UIView *)inView fullscreen:(BOOL)fullscreen;
 + (void)showView:(UIView *)view withFrame:(CGRect)frame inView:(UIView *)inView fullscreen:(BOOL)fullscreen;
 + (void)showImage:(UIImage *)image withFrame:(CGRect)frame inView:(UIView *)inView;
