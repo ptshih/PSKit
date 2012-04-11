2af753a5ddb9389831a2a219a35dfaa6a472ec55
Peter Shih <ptshih@gmail.com>
2012-04-11 02:23:09 -0400

    pstextview refactor

--- src/PSKit/PSTextView.m
+++ src/PSKit/PSTextView.m
@@ -8,7 +8,10 @@
 
 #import "PSTextView.h"
 
-@interface PSTextView (Private)
+@interface PSTextView ()
+
+@property (nonatomic, assign) UIEdgeInsets insets;
+@property (nonatomic, assign) UIEdgeInsets paddedInsets;
 
 - (void)updateShouldDrawPlaceholder;
 - (void)textChanged:(NSNotification *)notification;
@@ -17,100 +20,108 @@
 
 @implementation PSTextView
 
+@synthesize
+insets = _insets,
+paddedInsets = _paddedInsets;
+
 @synthesize placeholder = _placeholder;
 @synthesize placeholderColor = _placeholderColor;
 
 - (id)initWithFrame:(CGRect)frame {
-  self = [super initWithFrame:frame];
-  if (self) {
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:self];
-    
-    self.placeholderColor = [UIColor lightGrayColor];
-    _shouldDrawPlaceholder = NO;
-    
-    self.backgroundColor = [UIColor clearColor];
-  }
-  return self;
+    self = [super initWithFrame:frame];
+    if (self) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:self];
+        
+        self.insets = UIEdgeInsetsMake(-4, -8, 0, 0);
+        self.paddedInsets = UIEdgeInsetsMake(-4, -8, 8, 0);
+        self.contentInset = self.insets;
+        
+        self.placeholderColor = [UIColor lightGrayColor];
+        _shouldDrawPlaceholder = NO;
+        
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
 }
 
 
 - (void)drawRect:(CGRect)rect {
-  [super drawRect:rect];
-  
-  if (_shouldDrawPlaceholder) {
-    [_placeholderColor set];
-    [_placeholder drawInRect:CGRectMake(8.0, 8.0, self.frame.size.width - 16.0, self.frame.size.height - 16.0) withFont:self.font];
-  }
+    [super drawRect:rect];
+    
+    if (_shouldDrawPlaceholder) {
+        [_placeholderColor set];
+        [_placeholder drawInRect:CGRectMake(8.0, 0.0, self.frame.size.width - 16.0, self.frame.size.height) withFont:self.font];
+    }
 }
 
 
 #pragma mark Setters
 - (void)setText:(NSString *)string {
-  [super setText:string];
-  [self updateShouldDrawPlaceholder];
+    [super setText:string];
+    [self updateShouldDrawPlaceholder];
 }
 
 
 - (void)setPlaceholder:(NSString *)string {
-  if ([string isEqual:_placeholder]) {
-    return;
-  }
-  
-  [_placeholder release];
-  _placeholder = [string retain];
-  
-  [self updateShouldDrawPlaceholder];
+    if ([string isEqual:_placeholder]) {
+        return;
+    }
+    
+    [_placeholder release];
+    _placeholder = [string retain];
+    
+    [self updateShouldDrawPlaceholder];
 }
 
 
 #pragma mark Private Methods
 
 - (void)updateShouldDrawPlaceholder {
-  BOOL prev = _shouldDrawPlaceholder;
-  _shouldDrawPlaceholder = self.placeholder && self.placeholderColor && self.text.length == 0;
-  
-  if (prev != _shouldDrawPlaceholder) {
-    [self setNeedsDisplay];
-  }
+    BOOL prev = _shouldDrawPlaceholder;
+    _shouldDrawPlaceholder = self.placeholder && self.placeholderColor && self.text.length == 0;
+    
+    if (prev != _shouldDrawPlaceholder) {
+        [self setNeedsDisplay];
+    }
 }
 
 - (void)textChanged:(NSNotification *)notificaiton {
-  [self updateShouldDrawPlaceholder];    
+    [self updateShouldDrawPlaceholder];    
 }
 
 #pragma mark Content Inset/Offset
 - (void)setContentOffset:(CGPoint)s {
-	if(self.tracking || self.decelerating){
-		//initiated by user...
-		self.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
-	} else {
+    if(self.tracking || self.decelerating){
+        //initiated by user...
+        self.contentInset = self.insets;
+    } else {
+        
+        float bottomOffset = (self.contentSize.height - self.frame.size.height + self.contentInset.bottom);
+        if(s.y < bottomOffset && self.scrollEnabled){
+            self.contentInset = self.paddedInsets; //maybe use scrollRangeToVisible?
+        }
+        
+    }
     
-		float bottomOffset = (self.contentSize.height - self.frame.size.height + self.contentInset.bottom);
-		if(s.y < bottomOffset && self.scrollEnabled){
-			self.contentInset = UIEdgeInsetsMake(0, 0, 8, 0); //maybe use scrollRangeToVisible?
-		}
-		
-	}
-	
-	[super setContentOffset:s];
+    [super setContentOffset:s];
 }
 
 - (void)setContentInset:(UIEdgeInsets)s {
-	UIEdgeInsets insets = s;
-	
-	if(s.bottom>8) insets.bottom = 0;
-	insets.top = 0;
-  
-	[super setContentInset:insets];
+    UIEdgeInsets insets = s;
+    
+    if(s.bottom>8) insets.bottom = 0;
+    insets.top = 0;
+    
+    [super setContentInset:insets];
 }
 
 
 - (void)dealloc {
-  [[NSNotificationCenter defaultCenter] removeObserver:self name:UITextViewTextDidChangeNotification object:self];
-  
-  [_placeholder release], _placeholder = nil;
-  [_placeholderColor release], _placeholderColor = nil;
-
-  [super dealloc];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UITextViewTextDidChangeNotification object:self];
+    
+    [_placeholder release], _placeholder = nil;
+    [_placeholderColor release], _placeholderColor = nil;
+    
+    [super dealloc];
 }
 @end
