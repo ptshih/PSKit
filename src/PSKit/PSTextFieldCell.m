6387c6efc00340caffab6be6e0d9dd4e508494bc
Peter Shih <ptshih@gmail.com>
2013-04-19 14:12:22 -0700

    pstextfield

--- src/PSKit/PSTextFieldCell.m
+++ src/PSKit/PSTextFieldCell.m
@@ -30,24 +30,27 @@ static CGSize margin() {
         self.accessoryType = UITableViewCellAccessoryNone;
         self.selectionStyle = UITableViewCellSelectionStyleNone;
         
-        [PSStyleSheet applyStyle:@"leadDarkLabel" forLabel:self.textLabel];
-        
         self.textField = [[UITextField alloc] initWithFrame:CGRectZero];
         self.textField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
         self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
         self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
-//        self.textField.adjustsFontSizeToFitWidth = YES;
         self.textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-        [PSStyleSheet applyStyle:@"leadDarkField" forTextField:self.textField];
+        [self.textField addTarget:self action:@selector(textFieldChanged:) forControlEvents:UIControlEventEditingChanged];
+        [PSStyleSheet applyStyle:@"cellPlaceholderLabel" forTextField:self.textField];
         [self.contentView addSubview:self.textField];
     }
     return self;
+}
+
+- (void)textFieldChanged:(UITextField *)textField {
+    [self.object setObject:textField.text forKey:@"value"];
 }
 
 - (void)prepareForReuse {
     [super prepareForReuse];
     
     self.textField.text = nil;
+    self.textField.enabled = YES;
 }
 
 - (void)layoutSubviews {
@@ -56,29 +59,24 @@ static CGSize margin() {
     CGFloat left = margin().width;
     CGFloat top = 0.0;
     CGFloat width = self.contentView.width - margin().width * 2;
-    CGFloat height = self.contentView.height;
-    CGSize labelSize = CGSizeZero;
     
-    labelSize = [self.textLabel sizeForLabelInWidth:width];
-    self.textLabel.frame = CGRectMake(left, top, labelSize.width, height);
-    
-    left += MAX(labelSize.width, 100.0) + margin().width;
-    width -= MAX(labelSize.width, 100.0) + margin().width;
-    
-    self.textField.frame = CGRectMake(left, top, width, height);
+    self.textField.frame = CGRectMake(left, top, width, 44.0);
 }
 
 - (void)tableView:(UITableView *)tableView fillCellWithObject:(NSDictionary *)dict atIndexPath:(NSIndexPath *)indexPath {
     [super tableView:tableView fillCellWithObject:dict atIndexPath:indexPath];
     
-    NSString *title = [dict objectForKey:@"title"];
-    NSString *subtitle = [dict objectForKey:@"subtitle"];
-    
-    self.textLabel.text = title;
-    self.textField.placeholder = subtitle;
+    NSString *placeholder = [dict objectForKey:@"placeholder"];
+    self.textField.placeholder = placeholder;
+    if ([dict objectForKey:@"value"]) {
+        self.textField.text = [dict objectForKey:@"value"];
+    }
+    if ([dict objectForKey:@"enabled"] && [[dict objectForKey:@"enabled"] isEqualToString:@"false"]) {
+        self.textField.enabled = NO;
+    }
 }
 
-+ (CGFloat)rowHeightForObject:(NSDictionary *)dict atIndexPath:(NSIndexPath *)indexPath forInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
++ (CGFloat)rowHeightForObject:(id)object atIndexPath:(NSIndexPath *)indexPath forInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return 44.0;
 }
 
